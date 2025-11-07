0902f2f1defb58e714d4af0cb011b301956ab21a
Kien <141056025+kientran-dev@users.noreply.github.com>
2025-11-08 01:27:55 +0700

    feat: upload mainactivity

---
+++ app/src/main/java/com/starlms/starlms/entity/User.java
@@ -0,0 +1,18 @@
+package com.starlms.starlms.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+    String username;
+    String password;
+}
